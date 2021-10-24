0c1ac40437ea0fa1ec28f23996b3410dc6878fe9
Olapat <lapatbr19@gmail.com>
2021-10-24 12:56:10 +0700

    Init material design

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html data-theme='dark'>
+        <Head>
+          <meta name="description" content="Document react hook useForm Olapat" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
